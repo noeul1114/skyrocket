7cdc392eb7f7344569f7e96be6af465d618adf64
ezen <parkhyongsok@naver.com>
2020-02-24 17:32:04 +0900

    Update docker production settings

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.8.1-alpine
+
+RUN apt-get update
+
+RUN apt
--- skyrocket/settings/production.py
+++ skyrocket/settings/production.py
@@ -1,14 +1,28 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-SECRET_KEY = open( os.path.join(BASE_DIR,"env")).read().rstrip().lstrip()
+secret_root = '/run/secret/'
+
+secret_list = dict()
+all_secret_file = os.listdir(secret_root)
+
+for secret in all_secret_file:
+    file = open(os.path.join(secret_root, secret))
+    secret_list[secret] = file.read().lstrip().rstrip()
+    file.close()
+
+SECRET_KEY = secret_list['SECRET_KEY']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': secret_list['MYSQL_DATABASE'],
+        'USER': secret_list['MYSQL_USER'],
+        'PASSWORD': secret_list['MYSQL_PASSWORD'],
+        'HOST': secret_list['MYSQL_HOST'],
+        'PORT': '3306',
     }
 }
