498d9caa6a1422ecf1ead8074d1ee75041af2733
Park HS <parkhyongsok@naver.com>
2020-02-29 22:19:06 +0900

    Prepare to update user app with token

--- skyrocket/settings/base.py
+++ skyrocket/settings/base.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken'
+    'user',
     'stock',
     'core',
 ]
