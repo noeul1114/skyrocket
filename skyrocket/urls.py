9f41b6cabbc78a7d8904a3e13525f02d64e56cd6
Park HS <parkhyongsok@naver.com>
2020-03-01 00:30:30 +0900

    Added endpoint for creating and authenticating user

--- skyrocket/urls.py
+++ skyrocket/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stock.urls'))
+    path('', include('stock.urls')),
+    path('api/user/', include('user.urls')),
 ]
